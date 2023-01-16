848917bdd255893246f84edf8a8bd441a5cdabfb
Italo Nobre <italonobre@ads.fiponline.edu.br>
2023-01-16 17:10:17 -0300

    create viewSet for Course and Assessment

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Course, Assessment
+from .serializers import CourseSerializer,AssessmentSerializer
 
-# Create your views here.
+
+
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+class AssessmentViewSet(viewsets.ModelViewSet):
+    queryset = Assessment.objects.all()
+    serializer_class = AssessmentSerializer
