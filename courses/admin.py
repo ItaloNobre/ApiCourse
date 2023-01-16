28b4763a761be65cfd8b5e87504ea6e2067b7406
Italo Nobre <italonobre@ads.fiponline.edu.br>
2023-01-16 17:03:56 -0300

    register course and assessment

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Course, Assessment
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'url', 'create', 'update', 'active')
+
+@admin.register(Assessment)
+class AssessmentAdmin(admin.ModelAdmin):
+    list_display = ('id_course', 'name', 'email', 'assessment', 'create', 'update', 'active')
+
