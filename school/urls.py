fd76ff9a65d6d79a24a9661d0d0326806a7c90d8
Italo Nobre <italonobre@ads.fiponline.edu.br>
2023-01-16 17:09:41 -0300

    adding course.urls

--- school/urls.py
+++ school/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from courses.urls import router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('courses.urls'))
+    path('', include(router.urls))
 ]
